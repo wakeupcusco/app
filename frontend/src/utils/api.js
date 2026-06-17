c1883aafda573a6fabc4566e3f9737e5140e3814
wakeupcusco <167572400+wakeupcusco@users.noreply.github.com>
2026-06-16 19:54:08 -0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const API = `https://app-2-9ywg.onrender.com`;
+export const API = `https://app-2-9ywg.onrender.com/api`;
 
 const api = axios.create({
   baseURL: API,
